c8c97ded340d502593173fa79c4d292efd61bb8f
Elifarley <elifarley@gmail.com>
2024-10-07 10:27:24 -0300

    Fixed build script

--- build.sh
+++ build.sh
@@ -24,12 +24,14 @@ ts build --wasm && \
 cp -a test target && \
 ts test || { playground; exit 1 ;}
 
-mkdir -p vendor && \
-rm -f target/libtree-sitter-cedar.* vendor/libtree-sitter-cedar.* && \
+rm -f target/libtree-sitter-cedar.* cedarscript_grammar/libtree-sitter-cedar.* && \
 (cd target && cc -c -I./src src/parser.c && cc -dynamiclib -o libtree-sitter-cedar.dylib parser.o) && \
-mv target/libtree-sitter-cedar.dylib vendor/ && \
+mv target/libtree-sitter-cedar.dylib cedarscript_grammar/ && \
 docker build -t tree-sitter-cedar-builder . && \
 docker cp $(docker create tree-sitter-cedar-builder):/libtree-sitter-cedar.so \
-  vendor/libtree-sitter-cedar.so || exit
+  cedarscript_grammar/libtree-sitter-cedar.so || exit
+
+ls -Falk cedarscript_grammar/lib*
+du -hs cedarscript_grammar/lib*
 
 playground
